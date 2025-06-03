3488769be4776eaa59e14a208312a8565cbec82a
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-06-03 14:57:21 -0300

    feat(etch-a-sketch): add colorful and darkening modes
    - Colorful mode: randomize the squares' RGB values with each interaction.
    - Darkening mode: each interaction darkens the square by 10%.
    - It's possible to combine these modes.
    
    - Extra feature: while pressing shift key the user can erase squares.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,3 +1,10 @@
+function getRandomRGB(opacity=1.0) {
+    let red = parseInt(Math.random() * 1000 % 257);
+    let green = parseInt(Math.random() * 1000 % 257);
+    let blue = parseInt(Math.random() * 1000 % 257);
+    return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+}
+
 function createRow(rowHeight) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -5,39 +12,68 @@ function createRow(rowHeight) {
     return row;
 }
 
-function createSquare(width, squareBorder) {
+function createSquare(width) {
     let square = document.createElement("div");
     square.classList.add("square");
     square.style.width = square.style.height = width;
-    square.style.border = `${squareBorder}px solid lightgray`;
+    square.style.border = SQUARE_BORDER;
+    square.style.backgroundColor = EMPTY_COLOR;
     square.addEventListener("mouseenter", paintSquare);
     return square;
 }
 
-function paintSquare(event) {
-    event.target.style.backgroundColor = PAINTED_SQUARE_COLOR;
+function increaseOpacity(element) {
+    let red, green, blue, alpha;
+    [red, green, blue, alpha] = element.style.backgroundColor.match(/[0-9]+(.[0-9]+)?/g);
+    if (alpha === undefined) alpha = 1.0;
+    else if (alpha < 1.0) alpha = parseFloat(alpha) + 0.1;
+    element.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 }
 
-function addSquares(row, numberOfColumns, squareWidth, squareBorder) {
+function paintSquare(event) {
+    if (event.shiftKey) {
+        event.target.style.backgroundColor = EMPTY_COLOR;
+        return;
+    }
+
+    if (!colorful && !darkening) {
+        event.target.style.backgroundColor = BLACK_COLOR;
+    }
+    else if (!colorful && darkening) {
+        increaseOpacity(event.target);
+    }
+    else if (colorful && !darkening) {
+        event.target.style.backgroundColor = getRandomRGB();
+    }
+    else if (colorful && darkening) {
+        if (event.target.style.backgroundColor === EMPTY_COLOR)
+            event.target.style.backgroundColor = getRandomRGB(0.1);
+        else
+            increaseOpacity(event.target);
+    } 
+    
+}
+
+function addSquares(row, numberOfColumns, squareWidth) {
     for (let i = 0; i < numberOfColumns; i++) {
-        let square = createSquare(squareWidth, squareBorder);
+        let square = createSquare(squareWidth);
         row.appendChild(square);
     }
 }
 
-function createGrid(gridSize, squareBorder) {
+function createGrid(gridSize, squareBorderSize) {
     let grid = document.querySelector("div#grid");
     let gridStyle = getComputedStyle(grid);
 
     let rowHeight = `${parseInt(gridStyle.height) / gridSize}px`;
-    let squareWidth = `${(parseInt(gridStyle.width) / gridSize) - squareBorder}px`;
+    let squareWidth = `${(parseInt(gridStyle.width) / gridSize) - squareBorderSize}px`;
 
-    // reset grid
+    // Reset grid
     grid.innerHTML = "";
 
     for (let i = 0; i < gridSize; i++) {
         let row = createRow(rowHeight);
-        addSquares(row, gridSize, squareWidth, squareBorder);
+        addSquares(row, gridSize, squareWidth);
         grid.appendChild(row);
     }
 
@@ -45,15 +81,25 @@ function createGrid(gridSize, squareBorder) {
 
 function newGrid() {
     let gridSize = prompt("Enter new grid size:");
-    createGrid(gridSize, squareBorder);
+    colorful = confirm("Colorful mode?");
+    darkening = confirm("Darkening mode?");
+    createGrid(gridSize, squareBorderSize);
 }
 
-// Begin
-const initialGridSize = 16, squareBorder = 1;
-const EMPTY_SQUARE_COLOR = "white"; // "rgb(163, 163, 163)"
-const PAINTED_SQUARE_COLOR = "black"; // "rgb(24, 24, 24)"
 
-createGrid(initialGridSize, squareBorder);
+// Begin
+const EMPTY_COLOR = "rgba(0, 0, 0, 0)";
+const BLACK_COLOR = "rgba(0, 0, 0, 1)";
+const initialGridSize = 16
+const squareBorderSize = 1;
+const SQUARE_BORDER = `${squareBorderSize}px solid lightgrey`;
+
+let colorful = false;
+let darkening = false;
+
+createGrid(initialGridSize, squareBorderSize);
 
 let newGridButton = document.querySelector("button#new-grid-btn");
+let gridLinesButton = document.querySelector("button#grid-lines-btn");
+
 newGridButton.addEventListener("click", newGrid);
