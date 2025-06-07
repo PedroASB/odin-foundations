1d830067003c199e529d44f46ee1335f717edd57
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-06-07 19:07:44 -0300

    fix(calculator): prevent some issues due to wrong use
    - Pressing '=' at the wrong time (e.g. before entering all numbers) no longer causes problems.
    - If consecutive operator buttons are pressed, the calculator now only take the last operator entered to be used for the next operation.
    - Extra: pressing an operator after a result now uses the result as first operand.

--- calculator/script.js
+++ calculator/script.js
@@ -1,97 +1,203 @@
-function display(content) {
-    output.innerText = content;
+function displayOutput(content) {
+    document.querySelector("#output").innerText = content;
+}
+
+function clearOutput() {
+    document.querySelector("#output").innerText = "";
+}
+
+function displayHistory(content) {
+    document.querySelector("#history").innerText = content;
+}
+
+function clearHistory() {
+    document.querySelector("#history").innerText = "";
+}
+
+function clearCalculator() {
+    displayOutput(0);
+    clearHistory();
+    stack.clear();
+    hasResultBuffer = false;
 }
 
 function operate(firstOperand, secondOperand, operator) {
     let result;
 
+    /**
+     * @TODO refactor this function
+     */
+
     switch (operator) {
-        case "+":
+        case PLUS_OPERATOR:
             result = firstOperand + secondOperand;
             break;
-        case "-":
+        case MINUS_OPERATOR:
             result = firstOperand - secondOperand;
             break;
-        case "*":
+        case TIMES_OPERATOR:
             result = firstOperand * secondOperand;
             break;
-        case "/":
+        case DIVIDE_OPERATOR:
             result = secondOperand === 0? DIVISION_BY_ZERO_ERROR : firstOperand / secondOperand;
             break;
         default:
-            break;
+            return; // check this
     }
 
-    display(result);
-    currentNumber = 0;
+    return result;
+}
 
+function isNumber(item) {
+    return typeof(item) === "number";
+}
+
+function isOperator(item) {
+    switch (item) {
+        case PLUS_OPERATOR: 
+        case MINUS_OPERATOR: 
+        case TIMES_OPERATOR: 
+        case DIVIDE_OPERATOR:
+            return true;
+        default:
+            return false;
+    }
 }
 
 function appendDigit(event, digit) {
-    currentNumber = currentNumber * 10 + digit;
-    display(currentNumber);
-}
+    let currentNumber;
 
-function defineOperator(event, symbol) {
-    operator = symbol;
-    firstOperand = currentNumber;
-    currentNumber = 0;
-    // highlighOperator(event.target);
-}
-
-function highlighOperator(element) {
-    let operatorButtons = Array.from(document.querySelectorAll("button.operator"));
-    for (button of operatorButtons) {
-        button.style.backgroundColor = (element === button) ? 
-                                        HIGHLIGHTED_BUTTON_COLOR : 
-                                        DEFAULT_BUTTON_COLOR;
+    /**
+     * @TODO refactor this
+     */
+    if (hasResultBuffer && isNumber(stack.peek())) {
+        stack.pop();
+        currentNumber = 0;
+    } 
+    else if (stack.isEmpty() || isOperator(stack.peek())) {
+        currentNumber = 0;
     }
+    else if (isNumber(stack.peek())) {
+        currentNumber = stack.pop();
+    }
+
+    hasResultBuffer = false;
+
+
+    if (Number.isInteger(currentNumber)) {
+        // currentNumber is an integer
+        currentNumber = currentNumber * 10 + digit;
+    }
+    else {
+        // currentNumber is a float number
+
+        /**
+         * @TODO handle float numbers
+         */
+    }
+
+    stack.push(currentNumber);
+    displayOutput(currentNumber);
 }
 
-function initializeEventListeners() {
+function handleOperator(event, operator) {
+
+    if (isOperator(stack.peek())) {
+        stack.pop();
+        stack.push(operator);
+        return;
+    }
+
+    stack.push(operator);
+}
+
+function handleEquals() {
+    /**
+     * @TODO check if there are [number operator number] in the stack
+     */
+    if (stack.size() < 3) {
+        return;
+    }
+
+    let secondOperand = stack.pop();
+    let operator = stack.pop();
+    let firstOperand = stack.pop();
+
+    let result = operate(firstOperand, secondOperand, operator);
+
+    /**
+     * @TODO refactor this
+     */
+    if (result === DIVISION_BY_ZERO_ERROR) {
+        clearCalculator();
+        displayHistory(`${firstOperand} ${operator} ${secondOperand}`);
+        displayOutput(result);
+        return;
+    }
+
+    displayHistory(`${firstOperand} ${operator} ${secondOperand}`);
+    displayOutput(result);
+    
+    stack.push(result);
+
+    console.log("handleEquals: RESULT =", result);
+    hasResultBuffer = true;
+}
+
+function initializeCalculator() {
     let clearButton = document.querySelector("#clear");
-    clearButton.addEventListener("click", clearCalculator);
+    let plusButton = document.querySelector("#plus");
+    let minusButton = document.querySelector("#minus");
+    let timesButton = document.querySelector("#times");
+    let divideButton = document.querySelector("#divide");
+    let equalsButton = document.querySelector("#equals");
+
+    clearButton.addEventListener("mousedown", clearCalculator);
+    plusButton.addEventListener("mousedown", (event) => handleOperator(event, PLUS_OPERATOR));
+    minusButton.addEventListener("mousedown", (event) => handleOperator(event, MINUS_OPERATOR));
+    timesButton.addEventListener("mousedown", (event) => handleOperator(event, TIMES_OPERATOR));
+    divideButton.addEventListener("mousedown", (event) => handleOperator(event, DIVIDE_OPERATOR));
+    equalsButton.addEventListener("mousedown", handleEquals);
 
     for (let digit = 0; digit <= 9; digit++) {
         let numberButton = document.querySelector(`#digit-${digit}`);
-        numberButton.addEventListener("click", (event) => appendDigit(event, digit));
+        numberButton.addEventListener("mousedown", (event) => appendDigit(event, digit));
     }
 
-    let plusButton = document.querySelector("#plus");
-    let minusButton = document.querySelector("#minus");
-    let multiplyButton = document.querySelector("#multiply");
-    let divideButton = document.querySelector("#divide");
-
-    plusButton.addEventListener("click", (event) => defineOperator(event, "+") );
-    minusButton.addEventListener("click", (event) => defineOperator(event, "-"));
-    multiplyButton.addEventListener("click", (event) => defineOperator(event, "*"));
-    divideButton.addEventListener("click", (event) => defineOperator(event, "/"));
-
-    let equalsButton = document.querySelector("#equals");
-    equalsButton.addEventListener("click", (event) => {
-        secondOperand = currentNumber;
-        operate(firstOperand, secondOperand, operator);
-    });
-    
+    clearCalculator();
 }
 
 
-function clearCalculator() {
-    output.innerText = currentNumber = 0;
-    /**
-     * @TODO clear all operands / operator
-     */
-}
+// Global constants and variables
+const DIVISION_BY_ZERO_ERROR = "Error";
+const PLUS_OPERATOR = "+";
+const MINUS_OPERATOR = "-";
+const TIMES_OPERATOR = "*";
+const DIVIDE_OPERATOR = "/";
+let hasResultBuffer;
+let stack = {
+    content: [],
+    push(...items) {
+        this.content.push(...items);
+    },
+    pop() {
+        return this.content.pop();
+    },
+    size() {
+        return this.content.length;
+    },
+    peek(index=0) {
+        if (this.isEmpty() || index < 0 || index > this.size() - 1) return null;
+        return this.content[this.size() - 1 - index];
+    },
+    isEmpty() {
+        return this.size() === 0;
+    },
+    clear() {
+        this.content = [];
+    }
+};
 
 
 // Begin
-const DEFAULT_BUTTON_COLOR = "rgb(255, 255, 255)";
-const HIGHLIGHTED_BUTTON_COLOR = "rgb(255, 216, 194)";
-const DIVISION_BY_ZERO_ERROR = "Error";
-let firstOperand, secondOperand, operator;
-let currentNumber = 0;
-let output = document.querySelector("#output");
-
-
-clearCalculator();
-initializeEventListeners();
+initializeCalculator();
